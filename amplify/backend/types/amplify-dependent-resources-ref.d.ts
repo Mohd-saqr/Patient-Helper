86af0e2d40347bc40edbdebb6e9691953a6ebad4
msqer998 <saqerabu9@gmail.com>
2022-06-20 17:44:38 +0300

    add aws stuff

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,34 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "patianthelper": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "patianthelper90cb3cf8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3504a2c4e": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "S3Triggerfaa17fca": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
--- app/src/main/java/com/patient/patienthelper/activitys/Splash.java
+++ app/src/main/java/com/patient/patienthelper/activitys/Splash.java
@@ -3,14 +3,44 @@ package com.patient.patienthelper.activitys;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 import com.patient.patienthelper.R;
 
 public class Splash extends AppCompatActivity {
+
+    private static final String TAG = Splash.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        initializeAws();
     }
+
+
+
+
+    /**
+     * this method for initialize aws stuff
+     */
+    private void initializeAws() {
+        try {
+            Amplify.configure(getApplicationContext());
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
+            Log.i(TAG, "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e(TAG, "Could not initialize Amplify", error);
+        }
+    }
+
+
+
 }
