d47ab1b50c907a972a867e8bf197ffcd0a037e2a
A7mad3bd <ahmadabd398@gmail.com>
2022-06-22 00:34:04 +0300

    Lookinfor-Recyclerview

--- app/src/main/java/com/patient/patienthelper/activitys/LookingForActivity.java
+++ app/src/main/java/com/patient/patienthelper/activitys/LookingForActivity.java
@@ -2,15 +2,78 @@ package com.patient.patienthelper.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
 import com.patient.patienthelper.R;
 
 public class LookingForActivity extends AppCompatActivity {
+
+    Button button;
+    Spinner sp;
+    private static final String TAG = "";
+    SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_looking_for);
+        findViews();
+        SpinnerSelected();
+
     }
+
+    public void findViews() {
+        button = findViewById(R.id.submit_looking_for);
+        sp = (Spinner) findViewById(R.id.spinner_looking_for);
+    }
+
+    public void SpinnerSelected() {
+
+
+        String Selected = String.valueOf(sp.getSelectedItem());
+
+        shared();
+
+        if (Selected.equals("Patient")) {
+
+            System.out.println(Selected);
+            Log.i(TAG, "onCreate: " + Selected);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), Select_illActivity.class);
+                    startActivity(intent);
+                }
+            });
+        } else if (Selected.equals("Drug conflict")) {
+            System.out.println(Selected);
+            Log.i(TAG, "onCreate: " + Selected);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent1);
+                }
+            });
+        }
+    }
+
+    private void shared() {
+        sharedpreferences = getSharedPreferences("SHARED_PREF", MODE_PRIVATE);
+        String SelectedSpinner = sharedpreferences.getString("Selected", "");
+    }
+
+
 }
--- app/src/main/java/com/patient/patienthelper/activitys/Select_illActivity.java
+++ app/src/main/java/com/patient/patienthelper/activitys/Select_illActivity.java
@@ -1,16 +1,20 @@
 package com.patient.patienthelper.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.patient.patienthelper.R;
 
 public class Select_illActivity extends AppCompatActivity {
-
+    RecyclerView recyclerview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_ill);
+
+         recyclerview = findViewById(R.id.ILLRecyclerView);
+
     }
 }
