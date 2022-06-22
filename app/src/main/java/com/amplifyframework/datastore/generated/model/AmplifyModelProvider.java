db189cd787f6bb68b7f6182fd75c7a124edea52a
unknown <saqerabu9@gmail.com>
2022-06-22 20:58:18 +0300

    add in post and comment in schema

--- app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
@@ -13,7 +13,7 @@ import java.util.Set;
  */
 
 public final class AmplifyModelProvider implements ModelProvider {
-  private static final String AMPLIFY_MODEL_VERSION = "97847a5ae4ef6df0d76519308d788822";
+  private static final String AMPLIFY_MODEL_VERSION = "2cbc37f5ea48ac6525f44c8a91401e78";
   private static AmplifyModelProvider amplifyGeneratedModelInstance;
   private AmplifyModelProvider() {
     
@@ -34,7 +34,7 @@ public final class AmplifyModelProvider implements ModelProvider {
   @Override
    public Set<Class<? extends Model>> models() {
     final Set<Class<? extends Model>> modifiableSet = new HashSet<>(
-          Arrays.<Class<? extends Model>>asList(Post.class)
+          Arrays.<Class<? extends Model>>asList(Todo.class, Post.class, Comment.class)
         );
     
         return Immutable.of(modifiableSet);
